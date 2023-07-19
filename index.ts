67fd34d5dbe2b78febfb04b15fa916872130f8e3
DimitarVarzilov <dnvarzilov@diadraw.com>
2023-07-19 14:09:05 +0300

    db connection and initialization

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express, json } from "express";
 import dotenv from "dotenv";
+import { connectToDb } from "./src/db/dbInit";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
+connectToDb();
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
+app.use(json());
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
