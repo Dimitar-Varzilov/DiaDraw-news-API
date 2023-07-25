6281315c67c1a75da713fa61c71f9561c47ee807
DimitarVarzilov <dnvarzilov@diadraw.com>
2023-07-25 15:42:28 +0300

    check middleware

---
+++ src/middlewares/checkOwner.ts
@@ -0,0 +1,23 @@
+import { NextFunction, Request, Response } from "express";
+import { getNewsById } from "../models/News";
+import { ICombinedBody } from "../interfaces/request";
+
+export const checkOwnership = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const newsId = req.params.id;
+  const {
+    user: { id: userId },
+  } = req.body as ICombinedBody;
+
+  try {
+    const news = await getNewsById(newsId);
+    if (!news) return res.sendStatus(404);
+    if (news.created_by !== userId) return res.sendStatus(403);
+    next();
+  } catch (error) {
+    return res.sendStatus(500);
+  }
+};
